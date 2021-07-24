a850a28d1c096f34835c0da8e57613bcb118b613
nevilleomangi <31406314+nevilleomangi@users.noreply.github.com>
2021-07-24 16:21:28 +0300

    add localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,38 @@
 
-const note = document.querySelector('.note')
+const note_ = document.querySelector('.note')
 
-// add outgrow
+const storage = window.localStorage
+
+const restore = _ => {
+  const saved = storage.getItem('note')
+  if (note_ && saved) note_.value = saved
+}
+
+const save = text => {
+  storage.setItem('note', text)
+  console.log('Saved')
+}
+
+const handleInput = event => {
+  save(event.target.value)
+}
+
+const throttle = (func, limit) => {
+  let inThrottle
+  return function() {
+    const args = arguments
+    const context = this
+    if (!inThrottle) {
+      func.apply(context, args)
+      inThrottle = true
+      setTimeout(() => inThrottle = false, limit)
+    }
+  }
+}
+
+note_.addEventListener('input', throttle(handleInput, 500))
+
+// init - run on start
+;(_ => {
+  restore()
+})()
